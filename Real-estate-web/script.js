ba7b0d8ed1934e3f59a314ffeab537d7fce5404b
Amaan Saleem <amaan@saleem-ali.com>
2024-05-14 22:12:57 +0100

    popup now has transition effect when loading up page,

--- Real-estate-web/script.js
+++ Real-estate-web/script.js
@@ -2,6 +2,12 @@ const closeButton = document.getElementById('close-pop-up');
 const overlay = document.getElementById('overlay');
 const popUp = document.getElementById('pop-up');
 
+window.onload= () =>{
+  overlay.classList.add('active');
+  popUp.classList.add('active');
+}
+
+
 closeButton.addEventListener('click',  ()=> {
   overlay.classList.remove('active');
   popUp.classList.remove('active');  
